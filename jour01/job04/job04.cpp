0b4e9ac94dfa1ea1c6f4614335d21fe43778b9a6
theo-baccam <theo.baccam@laplateforme.io>
2024-04-29 14:38:31 +0200

    jour01/job04: addition entre deux entiers

---
+++ jour01/job04/job04.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main() {
+    int a = 0;
+    std::cout << "Veuillez donner un premier entier: ";
+    std::cin >> a;
+
+    int b = 0;
+    std::cout << "Veuillez donner un deuxième entier: ";
+    std::cin >> b;
+
+    std::cout << "La somme est égale à " << a + b << std::endl;
+
+    return 0;
+}
