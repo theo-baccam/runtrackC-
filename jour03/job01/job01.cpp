ae1a34ac57c91f985ead8f6428489cd47d5edaf5
theo-baccam <theo.baccam@laplateforme.io>
2024-05-01 12:20:37 +0200

    jour03/job01: utilisation de strlen à la place de sizeof

--- jour03/job01/job01.cpp
+++ jour03/job01/job01.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 // lettres minuscules vont de 97-122 inclus
 // lettres majuscules vont de 65-90 inclus (- 32)
@@ -7,7 +8,7 @@ int main() {
     char string[] = "vive la plateforme !";
     std::cout << string << std::endl;
 
-    for (int i = 0; i < sizeof(string); i++) {
+    for (int i = 0; i < strlen(string); i++) {
 
         if (!(97 <= string[i] && string[i] <= 122)) {
             continue;
