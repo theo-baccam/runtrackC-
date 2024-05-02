a1563f5bae680dfcbc3b817bb6d7d7c792d649d8
theo-baccam <theo.baccam@laplateforme.io>
2024-05-02 09:48:36 +0200

    jour03/job07: renommé integerArray à T (comme la consigne spécifie)

--- jour03/job07/job07.cpp
+++ jour03/job07/job07.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 
 int main() {
-    int integerArrayLength = 10;
-    int integerArray[integerArrayLength];
+    int TLength = 10;
+    int T[TLength];
 
     std::cout 
         << "Veuillez entrer 10 entiers "
         "(ENTREE entre chaque entiers)"
         << std::endl;
 
-    for (int i = 0; i < integerArrayLength; i++) {
-        std::cin >> integerArray[i];
+    for (int i = 0; i < TLength; i++) {
+        std::cin >> T[i];
     };
 
-    int greatestInteger = integerArray[0];
+    int greatestInteger = T[0];
     int greatestIntegerIndex = 0;
-    for (int i = 0; i < integerArrayLength; i++) {
-        if (integerArray[i] > greatestInteger) {
-            greatestInteger = integerArray[i];
+    for (int i = 0; i < TLength; i++) {
+        if (T[i] > greatestInteger) {
+            greatestInteger = T[i];
             greatestIntegerIndex = i;
         };
     };
