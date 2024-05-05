617313a43ab6e2bcbc8df7ebbc9c932483047d9b
theo-baccam <theo.baccam@laplateforme.io>
2024-05-05 20:10:30 +0200

    jour05/job01: classe joueur

---
+++ jour05/job01/job01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "job01.hpp"
+
+
+Joueur::Joueur(double initialX, double initialY) {
+    x = initialX;
+    y = initialY;
+}
+
+Joueur::~Joueur() {
+    std::cout << "Joueur detruit" << std::endl;
+}
+
+void Joueur::setX(double newX) {
+    x = newX;
+}
+
+void Joueur::setY(double newY) {
+    y = newY;
+}
+
+
+int main() {
+    Joueur johnDoe(32.58, -12.93);
+    std::cout << johnDoe.x << " " << johnDoe.y << std::endl;
+
+    johnDoe.setX(johnDoe.x + 1.5);
+    johnDoe.setY(johnDoe.y + 1.5);
+    std::cout << johnDoe.x << " " << johnDoe.y << std::endl;
+
+    return 0;
+}
---
+++ jour05/job01/job01.hpp
@@ -0,0 +1,9 @@
+class Joueur {
+    public:
+        Joueur(double initialX, double initialY);
+        ~Joueur();
+        double x;
+        double y;
+        void setX(double newX);
+        void setY(double setY);
+};
